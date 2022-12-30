94db4e2abdbc9b400fbd52027645cc68929f2c9d
ABinekar <anantbinekar@gmail.com>
2022-12-30 20:53:06 +0530

    day 1

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -5,6 +5,14 @@ class LoginPage:
 
     link_signInLogin_xpath = "//*[@id='header']/div/div/div/div[2]/div/ul/li[4]/a"
     text_NewUserSignUp_xpath = "//*[@id='form']/div/div/div[3]/div/h2"
+    textbox_SignInName_xpath = ""
+    textbox_SignInEmail_xpath = ""
+    btn_Signup_xpath = ""
+    textbox_LoginEmailAddress_xpath = ""
+    textbox_LoginPassword_xpath = ""
+    text_LoginToYourAccount_xpath = ""
+    btn_Login_xpath = ""
+    text_YourEmailOrPasswordIsIncorrect_xpath = ""
 
     def __init__(self, driver):
         self.driver = driver
